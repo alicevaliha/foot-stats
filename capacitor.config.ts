f73cf6462a3eddabdbd6186d5818feedfc7e6916
alicevaliha <alicevaliha@gmail.com>
2023-12-03 20:32:55 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'foot-stats',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
