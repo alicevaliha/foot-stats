1e1a891fda64c3bcd7baca652d52d1e7abf36ee0
alicevaliha <alicevaliha@gmail.com>
2023-12-04 00:15:22 +0300

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http'
 
+import {DataTablesModule} from 'angular-datatables'
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,DataTablesModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
